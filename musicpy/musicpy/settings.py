d1701fb8364091ea85d873bdf3beee0b52977e2e
Pablo Santos Cabaleiro <73109908+pablosantos1106@users.noreply.github.com>
2021-12-14 17:23:15 +0100

    Update settings.py

--- musicpy/musicpy/settings.py
+++ musicpy/musicpy/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'i#=si^eyyb8(-qle%dp-_5=ds9t8*w+tt@nd6(aoyr*pqao2j+'
+SECRET_KEY = '***************************************************'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
